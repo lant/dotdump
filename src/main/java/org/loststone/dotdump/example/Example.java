ee20a6c41f66e5bcec4cbcfc7989658e31a2eca7
Marc de Palol <marc.depalol@schibsted.com>
2017-06-20 14:51:08 +0200

    Now with shapes.

--- src/main/java/org/loststone/dotdump/example/Example.java
+++ src/main/java/org/loststone/dotdump/example/Example.java
@@ -1,9 +1,6 @@
 package org.loststone.dotdump.example;
 
-import org.loststone.dotdump.Dot;
-import org.loststone.dotdump.GraphNode;
-import org.loststone.dotdump.GraphType;
-import org.loststone.dotdump.NodeRelation;
+import org.loststone.dotdump.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,13 +11,13 @@ public class Example {
     Dot musicGraph = new Dot(GraphType.DIGRAPH, "Some metal bands");
     musicGraph.withComment("Some metal bands to draw a graph");
 
-    GraphNode maiden = new GraphNode("iron-maiden").withText("Iron Maiden");
-    GraphNode sabbath = new GraphNode("b-sabbath").withText("Black Sabbath");
-    GraphNode acdc = new GraphNode("acdc").withText("AC-DC");
-    GraphNode mayhem = new GraphNode("mayhem").withText("Mayhem");
-    GraphNode venom = new GraphNode("venom").withText("Venom");
-    GraphNode nasum = new GraphNode("nasum").withText("Nasum");
-    GraphNode slayer = new GraphNode("slayer").withText("Slayer");
+    GraphNode maiden = new GraphNode("iron-maiden").withText("Iron Maiden").withShape(NodeShapes.box);
+    GraphNode sabbath = new GraphNode("b-sabbath").withText("Black Sabbath").withShape(NodeShapes.circle);
+    GraphNode acdc = new GraphNode("acdc").withText("AC-DC").withShape(NodeShapes.diamond);
+    GraphNode mayhem = new GraphNode("mayhem").withText("Mayhem").withShape(NodeShapes.ellipse);
+    GraphNode venom = new GraphNode("venom").withText("Venom").withShape(NodeShapes.house);
+    GraphNode nasum = new GraphNode("nasum").withText("Nasum").withShape(NodeShapes.lpromoter);
+    GraphNode slayer = new GraphNode("slayer").withText("Slayer").withShape(NodeShapes.box3d);
 
     musicGraph.withNode(maiden);
     musicGraph.withNode(sabbath);
